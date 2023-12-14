16e4cd0809d95f5d7a4af7057867344983abddbf
Your Name <you@example.com>
2023-12-14 19:09:14 +0700

    Update

--- src/client/components/data/minigame/wheel/luckyUser.vue
+++ src/client/components/data/minigame/wheel/luckyUser.vue
@@ -1,7 +1,7 @@
 <template>
   <UCard :ui="{ 
     header: { padding: 'px-3 sm:px-3 py-2 sm:py-2' },
-    body: { padding: 'p-4 sm:p-4' },
+    body: { padding: 'p-0 sm:p-0' },
   }">
     <template #header>
       <UiText color="gray" weight="semibold">May mắn nhất</UiText>
@@ -13,23 +13,24 @@
       <DataEmpty text="Chưa có người may mắn" icon="i-bxs-color" v-if="list.length == 0" />
 
       <UiFlex type="col" class="gap-y-2" v-else>
-        <UNotification
+        <UAlert
           v-for="(item, index) in list" :key="index"
-          :close-button="{ icon: 'i-bxs-circle', size: 'xs', color: 'primary' }"
           :avatar="{ src: item.user.avatar, alt: item.user.username }"
-          :ui="{ background: 'dark:bg-gray-800' }"
-          :id="`wheel-luckyuser-${item._id}`"
-          :timeout="0"
+          variant="soft"
         >
           <template #title>
-            <UiText color="primary" weight="bold" class="capitalize">{{ item.user?.username || '...' }}</UiText>
+            <UiFlex justify="between">
+              <UiText color="primary" weight="bold" class="capitalize">{{ item.user?.username || '...' }}</UiText>
+              <UiText color="gray" size="xs">{{ useDayJs().fromTime(item.createdAt, null) }}</UiText>
+            </UiFlex>
           </template>
+
           <template #description>
             <span class="mr-1">Tôi vừa quay được</span>
             <span v-html="item.action" class="text-primary"></span>
             <span>{{ randSuffix() }}</span>
           </template>
-        </UNotification>
+        </UAlert>
       </UiFlex>
     </div>
   </UCard>
