ecdf5368b25e690186bace70af1c37c874682265
Your Name <you@example.com>
2023-12-18 14:37:53 +0700

    Update

--- src/client/components/data/banner.vue
+++ src/client/components/data/banner.vue
@@ -3,7 +3,7 @@
     <div class="relative"> 
       <UiImg 
         :src="config.og_image"
-        w="16" h="7"
+        w="16" h="9"
         img-size="xs:300px sm:600 md:800px 1000px"
         alt="Banner"
         class="transition-all rounded-xl shadow-md hover:shadow-lg"
