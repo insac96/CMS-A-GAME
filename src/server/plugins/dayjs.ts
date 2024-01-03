16687312bc2843edc76a238f0def0acb04393133
Your Name <you@example.com>
2024-01-03 19:54:32 +0700

    Update

--- src/server/plugins/dayjs.ts
+++ src/server/plugins/dayjs.ts
@@ -1,15 +1,15 @@
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
+// import utc from 'dayjs/plugin/utc'
+// import timezone from 'dayjs/plugin/timezone'
 
 declare global {
   var DayJS : any
 }
 
 export default defineNitroPlugin(async (nitroApp) => {
-  dayjs.extend(utc)
-  dayjs.extend(timezone)
-  dayjs.tz.setDefault('Asia/Ho_Chi_Minh')
+  // dayjs.extend(utc)
+  // dayjs.extend(timezone)
+  // dayjs.tz.setDefault('Asia/Ho_Chi_Minh')
 
   global.DayJS = dayjs
 })
