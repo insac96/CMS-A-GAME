e591ad0b085791c442fe67591dd131d43631c053
Your Name <you@example.com>
2024-02-03 06:45:02 +0700

    Update

--- src/server/utils/gameSendMail.ts
+++ src/server/utils/gameSendMail.ts
@@ -15,7 +15,7 @@ interface ISendData {
 
 export default async (event: H3Event, data : ISendData) : Promise<void> => {
   try {
-    return Promise.resolve()
+    //return Promise.resolve()
 
     const config = await DB.Config.findOne().select('game') as IDBConfig
     if(!config) throw 'Không tìm thấy cấu hình trò chơi'
