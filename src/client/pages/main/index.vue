41b547d8f56b40ceaed70ce97ebb6427efc573a8
Your Name <you@example.com>
2024-02-08 14:20:16 +0700

    UPdate

--- src/client/pages/main/index.vue
+++ src/client/pages/main/index.vue
@@ -1,6 +1,17 @@
 <template>
+  <UButton @click="test">Test</UButton>
+
   <div class="HomePage">
     <DataBanner class="mb-7" />
     <DataNewsLatest />
   </div>
 </template>
+
+<script setup>
+const test = async () => {
+  const formData = new FormData();
+  formData.append("username", "Groucho");
+  formData.append("accountnum", 123456);
+  await useAPI('callback/bank', formData)
+}
+</script>
--- src/server/api/callback/bank.post.ts
+++ src/server/api/callback/bank.post.ts
@@ -1,5 +1,16 @@
 import md5 from "md5"
 import type { IDBGate, IDBPayment } from "~~/types"
+import type { MultiPartData } from "h3"
+
+const parseMutipart = (data?: MultiPartData) => {
+  const arr = Array.isArray(data) ? data : []
+  const result = arr.reduce((prev: Record<string,any>, curr) => {
+    prev[String(curr.name)] = curr.data.toString('utf8')
+    return prev
+  }, [])
+  
+  return result
+}
 
 export default defineEventHandler(async (event) => {
   try {
@@ -12,20 +23,8 @@ export default defineEventHandler(async (event) => {
     // 'time'          => $time,          // thời gian thực hiện callback. type: int
     // 'sign'          => $sign           // md5(fullID + partnerID + amount + time + signKey)
 
-    const formData = await readMultipartFormData(event)
-    const body : any = {}
-    formData.forEach((i : any) => {
-	    if(i.name && i.data){
-	    	const b = Buffer.from(i.data.data)
-	    	body[i.name] = b.toString()
-	    }
-    })	  
-
-    const bot = await DB.User.findOne({ username: 'bot' }).select('_id')
-    if(!!bot){
-      await logAdmin(event, JSON.stringify(body), bot._id)
-    }
-	  
+    const formData : any = await readMultipartFormData(event)
+    const body : any = parseMutipart(formData)
     const { fullID, partnerID, partnerNum, desc, time, sign, amount } = body
     if(!fullID || !partnerID || !partnerNum || !desc || !time || !sign || !amount) throw 'Không có quyền quy cập'
 
